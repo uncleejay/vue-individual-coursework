108e685dd063164fe905f9ebc08c83939d1ddff6
uncleejay <joseph.c.o216@gmail.com>
2022-03-01 15:45:31 +0000

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -28,37 +28,3 @@ self.addEventListener('install', (e) => {
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (e)  {
-            return r
-        })
-    )
-});
-
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-    // check if the cache has the file
-    caches.match(e.request).then(function (r) {
-        console.log('[Service Worker] Fetching resource: ' + e.request.url);
-    // 'r' is the matching file if it exists in the cache
-    return r
-        })
-    );
-});
-
-
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-    caches.match(e.request).then(function (r) {
-    // Download the file if it is not in the cache,
-        return r || fetch(e.request).then(function (response) {
-    // add the new file to cache
-        return caches.open(cacheName).then(function (cache) {
-        cache.put(e.request, response.clone());
-        return response;
-            });
-        });
-    })
-);
-});
